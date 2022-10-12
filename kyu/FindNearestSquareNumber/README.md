3db4cf56b339c1e9282e244ba78cd79f7e60cdc8
Elija33 <amponsaeli@gmail.com>
2022-10-12 04:19:02 -0400

    Find nearest square number

---
+++ kyu/FindNearestSquareNumber/README.md
@@ -0,0 +1,8 @@
+# CodeWar
+                                        # If you can't sleep, just count sheep!!
+
+Task:
+
+Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+Goodluck :)
--- kyu/Powersof2/Powers2.java
+++ kyu/Powersof2/Powers2.java
@@ -2,7 +2,9 @@ package Powersof2;
 
 public class Powers2 {
     public static long[] powersOfTwo(int number) {
-
+        for (int i = 0; i < number; i++) {
+            return Math.pow(number, i);
+        }
     }
 
     public static void main(String[] args) {
